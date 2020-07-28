ead8f12b2291ec7121d52fced0bd01a53afb9a4a
lc <540562765@qq.com>
2020-07-28 15:33:26 +0800

    罗诚：增加了客户模块的三个部分——客户信息，客户消费次数，客户每次消费金额。

---
+++ src/components/common/HomeFoot.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<footer class="main-footer">
+		  <div class="pull-right hidden-xs">
+		    <b>Version</b> 1.0
+		  </div>
+		  <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">企业实训小组</a>.</strong> 版权所有.
+		</footer>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"HomeFooter",
+		data() {
+			return {};
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/components/customer/view.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+	export default {
+		name:"ConsumptionView",
+		data() {
+			return {};
+		}
+	}
+</script>
+
+<style>
+</style>
